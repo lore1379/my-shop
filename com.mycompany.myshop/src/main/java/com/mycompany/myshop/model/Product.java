7a8ac2333ad072fa88709d31b5156d70ec8314fc
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-28 10:06:58 +0200

    Domain model class Product

--- com.mycompany.myshop/src/main/java/com/mycompany/myshop/model/Product.java
+++ com.mycompany.myshop/src/main/java/com/mycompany/myshop/model/Product.java
@@ -1,5 +1,55 @@
 package com.mycompany.myshop.model;
 
-public class Product {
+import java.util.Objects;
 
+public class Product {
+	
+	private String id;
+	private String name;
+	
+	public Product() {}
+
+	public Product(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + "]";
+	}
+	
 }
