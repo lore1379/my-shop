7c2d681b489fd1aa7e81bc398c4f485ede73702b
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-28 08:44:03 +0200

    ShopSwingAppBDD created

---
+++ com.mycompany.myshop/src/bdd/java/com/mycompany/myshop/bdd/ShopSwingAppBDD.java
@@ -0,0 +1,12 @@
+package com.mycompany.myshop.bdd;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/bdd/resources", monochrome = true)
+public class ShopSwingAppBDD {
+
+}
