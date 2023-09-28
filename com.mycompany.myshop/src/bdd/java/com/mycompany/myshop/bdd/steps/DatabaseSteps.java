7be102743fda232b12ea9778e0936ab05ce59ddc
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-28 08:55:47 +0200

    Created steps file with copied methods

---
+++ com.mycompany.myshop/src/bdd/java/com/mycompany/myshop/bdd/steps/DatabaseSteps.java
@@ -0,0 +1,13 @@
+package com.mycompany.myshop.bdd.steps;
+
+import io.cucumber.java.en.Given;
+
+public class DatabaseSteps {
+	
+	@Given("The database contains a few products")
+	public void the_database_contains_a_few_products() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
---
+++ com.mycompany.myshop/src/bdd/java/com/mycompany/myshop/bdd/steps/ShopSwingViewSteps.java
@@ -0,0 +1,33 @@
+package com.mycompany.myshop.bdd.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ShopSwingViewSteps {
+	
+	@Given("The Shop View is shown")
+	public void the_Shop_View_is_shown() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user selects a product from the shop list")
+	public void the_user_selects_a_product_from_the_shop_list() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the {string} button")
+	public void the_user_clicks_the_button(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The cart list contains the product")
+	public void the_cart_list_contains_the_product() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
