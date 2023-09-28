a691ba9992642b35b60b251e6153e2b662bc51e3
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-28 10:44:12 +0200

    Implemented controller's allProducts

--- com.mycompany.myshop/src/main/java/com/mycompany/myshop/controller/ShopController.java
+++ com.mycompany.myshop/src/main/java/com/mycompany/myshop/controller/ShopController.java
@@ -1,17 +1,24 @@
 package com.mycompany.myshop.controller;
 
+import java.util.List;
+
+import com.mycompany.myshop.model.Product;
 import com.mycompany.myshop.repository.ShopRepository;
 import com.mycompany.myshop.view.ShopView;
 
 public class ShopController {
 
+	private ShopView shopView;
+	private ShopRepository shopRepository;
+
 	public ShopController(ShopView shopView, ShopRepository shopRepository) {
-		// TODO Auto-generated constructor stub
+		this.shopView = shopView;
+		this.shopRepository = shopRepository;
 	}
 
 	public void allProducts() {
-		// TODO Auto-generated method stub
-		
+		List<Product> products = shopRepository.findAll();
+		shopView.showAllProducts(products);
 	}
 
 }
