5b5d7a9131ed61682ac72fc0a2ca6700a7ee1ae5
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-29 09:53:17 +0200

    Implemented findAllProducts when database is not empty

--- com.mycompany.myshop/src/main/java/com/mycompany/myshop/repository/mongo/ShopMongoRepository.java
+++ com.mycompany.myshop/src/main/java/com/mycompany/myshop/repository/mongo/ShopMongoRepository.java
@@ -1,22 +1,34 @@
 package com.mycompany.myshop.repository.mongo;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.bson.Document;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mycompany.myshop.model.Cart;
 import com.mycompany.myshop.model.Product;
 import com.mycompany.myshop.repository.ShopRepository;
 
 public class ShopMongoRepository implements ShopRepository {
 
-	public ShopMongoRepository(MongoClient mongoClient, String string, String string2, String string3) {
-		// TODO Auto-generated constructor stub
+
+	private MongoCollection<Document> productCollection;
+
+	public ShopMongoRepository(MongoClient mongoClient, String databaseName, String firstCollectionName, String secondCollectionName) {
+		productCollection = mongoClient
+				.getDatabase(databaseName)
+				.getCollection(firstCollectionName);
 	}
 
 	@Override
 	public List<Product> findAllProducts() {
-		return Collections.emptyList();
+		return StreamSupport
+				.stream(productCollection.find().spliterator(), false)
+				.map(d -> new Product("" + d.get("id"), "" + d.get("name")))
+				.collect(Collectors.toList());
 	}
 
 	@Override
