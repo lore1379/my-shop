57173c6badff73b06f1047d05b5635c31b0854e6
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-29 09:22:51 +0200

    Refactor database setup + added cart collection to ShopMongoRepository

--- com.mycompany.myshop/src/main/java/com/mycompany/myshop/repository/mongo/ShopMongoRepository.java
+++ com.mycompany.myshop/src/main/java/com/mycompany/myshop/repository/mongo/ShopMongoRepository.java
@@ -4,7 +4,7 @@ import com.mongodb.MongoClient;
 
 public class ShopMongoRepository {
 
-	public ShopMongoRepository(MongoClient mongoClient, String string, String string2) {
+	public ShopMongoRepository(MongoClient mongoClient, String string, String string2, String string3) {
 		// TODO Auto-generated constructor stub
 	}
 
--- com.mycompany.myshop/src/test/java/com/mycompany/myshop/repository/mongo/ShopMongoRepositoryTest.java
+++ com.mycompany.myshop/src/test/java/com/mycompany/myshop/repository/mongo/ShopMongoRepositoryTest.java
@@ -3,30 +3,58 @@ package com.mycompany.myshop.repository.mongo;
 import java.net.InetSocketAddress;
 
 import org.bson.Document;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mycompany.myshop.repository.ShopRepository;
-
 import de.bwaldvogel.mongo.MongoServer;
 import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 
 public class ShopMongoRepositoryTest {
 	
-	@Test
-	public void testDBSetup() {
-		MongoServer server = new MongoServer(new MemoryBackend());
-		InetSocketAddress serverAddress = server.bind();
-		MongoClient mongoClient = new MongoClient(new ServerAddress(serverAddress));
-		ShopMongoRepository productRepository = new ShopMongoRepository(mongoClient, "shop", "product");
+	private static MongoServer server;
+	private static InetSocketAddress serverAddress;
+	
+	private MongoClient mongoClient;
+	private ShopMongoRepository shopRepository;
+	private MongoCollection<Document> productCollection;
+	private MongoCollection<Document> cartCollection;
+	
+	@BeforeClass
+	public static void setupServer() {
+		server = new MongoServer(new MemoryBackend());
+		serverAddress = server.bind();		
+	}
+	
+	@AfterClass
+	public static void shutdownServer() {
+		server.shutdown();
+	}
+	
+	@Before
+	public void setup() {
+		mongoClient = new MongoClient(new ServerAddress(serverAddress));
+		shopRepository = new ShopMongoRepository(mongoClient, "shop", "product", "cart");
 		MongoDatabase database = mongoClient.getDatabase("shop");
 		database.drop();
-		MongoCollection<Document> productCollection = database.getCollection("product");
+		productCollection = database.getCollection("product");
+		cartCollection = database.getCollection("cart");
+	}
+	
+	@After
+	public void tearDown() {
 		mongoClient.close();
-		server.shutdown();
+	}
+	
+	@Test
+	public void testDBSetup() {
+
 	}
 
 }
