1a64a20e44df0894fc031a00cb296e207edcf11d
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-10-02 09:47:59 +0200

    Implemented productList initial state

--- com.mycompany.myshop/src/main/java/com/mycompany/myshop/view/swing/ShopSwingView.java
+++ com.mycompany.myshop/src/main/java/com/mycompany/myshop/view/swing/ShopSwingView.java
@@ -1,11 +1,17 @@
 package com.mycompany.myshop.view.swing;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JList;
+import java.awt.Insets;
+import javax.swing.ListSelectionModel;
+import javax.swing.JScrollPane;
 
 public class ShopSwingView extends JFrame {
 
@@ -35,8 +41,32 @@ public class ShopSwingView extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblShop = new JLabel("Shop");
+		GridBagConstraints gbc_lblShop = new GridBagConstraints();
+		gbc_lblShop.insets = new Insets(0, 0, 5, 0);
+		gbc_lblShop.gridx = 0;
+		gbc_lblShop.gridy = 0;
+		contentPane.add(lblShop, gbc_lblShop);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 1;
+		contentPane.add(scrollPane, gbc_scrollPane);
+		
+		JList list = new JList();
+		scrollPane.setViewportView(list);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setName("productList");
 	}
 
 }
