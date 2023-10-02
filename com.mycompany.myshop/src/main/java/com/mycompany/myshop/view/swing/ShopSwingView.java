6f28983d0b22d94e7530e5df9e4dc18142693b03
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-10-02 09:50:21 +0200

    Refactor - View's Title and serial version ID

--- com.mycompany.myshop/src/main/java/com/mycompany/myshop/view/swing/ShopSwingView.java
+++ com.mycompany.myshop/src/main/java/com/mycompany/myshop/view/swing/ShopSwingView.java
@@ -15,6 +15,10 @@ import javax.swing.JScrollPane;
 
 public class ShopSwingView extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	/**
@@ -37,6 +41,7 @@ public class ShopSwingView extends JFrame {
 	 * Create the frame.
 	 */
 	public ShopSwingView() {
+		setTitle("MyShop");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
