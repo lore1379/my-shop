b2b23c96466c1cba6fb396f0ef8663492c642e1a
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-10-05 12:17:29 +0200

    BDD implemented "The database contains a few products"

--- com.mycompany.myshop/src/bdd/java/com/mycompany/myshop/bdd/steps/DatabaseSteps.java
+++ com.mycompany.myshop/src/bdd/java/com/mycompany/myshop/bdd/steps/DatabaseSteps.java
@@ -1,13 +1,55 @@
 package com.mycompany.myshop.bdd.steps;
 
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 
 public class DatabaseSteps {
 	
+	private static int mongoPort =
+			Integer.parseInt(System.getProperty("mongo.port", "27017"));
+	
+	private MongoClient mongoClient;
+	
+	private static final String DB_NAME = "test-db";
+	private static final String PRODUCT_COLLECTION_NAME = "test-product-collection";
+	
+	private static final String PRODUCT_FIXTURE_1_ID = "1";
+	private static final String PRODUCT_FIXTURE_1_NAME = "test1";
+	private static final String PRODUCT_FIXTURE_2_ID = "2";
+	private static final String PRODUCT_FIXTURE_2_NAME = "test2";
+	
+	@Before
+	public void setUp() {
+		mongoClient = new MongoClient(
+				new ServerAddress("localhost", mongoPort));
+		mongoClient.getDatabase(DB_NAME).drop();
+	}
+
+	@After
+	public void tearDown() {
+		mongoClient.close();
+	}
+	
 	@Given("The database contains a few products")
 	public void the_database_contains_a_few_products() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    addTestProductToDatabase(PRODUCT_FIXTURE_1_ID, PRODUCT_FIXTURE_1_NAME);
+	    addTestProductToDatabase(PRODUCT_FIXTURE_2_ID, PRODUCT_FIXTURE_2_NAME);
+	}
+	
+	private void addTestProductToDatabase(String id, String name) {
+		mongoClient
+			.getDatabase(DB_NAME)
+			.getCollection(PRODUCT_COLLECTION_NAME)
+			.insertOne(
+					new Document()
+					.append("id", id)
+					.append("name", name));
 	}
 
 }
