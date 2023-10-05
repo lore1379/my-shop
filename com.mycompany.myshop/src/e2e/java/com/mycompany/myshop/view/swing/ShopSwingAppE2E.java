5d69302f43f1af99717c611f386047c4f83d6e0e
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-10-05 11:54:53 +0200

    E2E test elements are shown in database on start

--- com.mycompany.myshop/src/e2e/java/com/mycompany/myshop/view/swing/ShopSwingAppE2E.java
+++ com.mycompany.myshop/src/e2e/java/com/mycompany/myshop/view/swing/ShopSwingAppE2E.java
@@ -1,13 +1,16 @@
 package com.mycompany.myshop.view.swing;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.swing.launcher.ApplicationLauncher.application;
 import javax.swing.JFrame;
 
+import org.assertj.swing.annotation.GUITest;
 import org.assertj.swing.core.GenericTypeMatcher;
 import org.assertj.swing.finder.WindowFinder;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.bson.Document;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -19,12 +22,18 @@ public class ShopSwingAppE2E extends AssertJSwingJUnitTestCase {
 	
 	private static int mongoPort =
 			Integer.parseInt(System.getProperty("mongo.port", "27017"));
+	
+	private MongoClient mongoClient;
+
+	private FrameFixture window;
 
 	@Override
 	protected void onSetUp() throws Exception {
-		MongoClient mongoClient = new MongoClient(
+		mongoClient = new MongoClient(
 				new ServerAddress("localhost", mongoPort));
 		mongoClient.getDatabase("shop").drop();
+		addTestProductToDatabase("1", "test1");
+		addTestProductToDatabase("2", "test2");
 		application("com.mycompany.myshop.app.swing.ShopSwingApp")
 			.withArgs(
 				"--mongo-host=" + "localhost",
@@ -34,15 +43,34 @@ public class ShopSwingAppE2E extends AssertJSwingJUnitTestCase {
 				"--cart-collection=" + "cart"
 			)
 			.start();
-		FrameFixture window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
+		window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
 			@Override
 			protected boolean isMatching(JFrame frame) {
 				return "MyShop".equals(frame.getTitle()) && frame.isShowing();
 			}
 		}).using(robot());
 	}
+	
 
-	@Test
-	public void test() {
+	@Override
+	protected void onTearDown() {
+		mongoClient.close();
+	}
+
+	@Test @GUITest
+	public void testOnStartAllDatabaseElementsAreShown() {
+		assertThat(window.list("productList").contents())
+			.anySatisfy(e -> assertThat(e).contains("1", "test1"))
+			.anySatisfy(e -> assertThat(e).contains("2", "test2"));
+	}
+	
+	private void addTestProductToDatabase(String id, String name) {
+		mongoClient
+			.getDatabase("shop")
+			.getCollection("product")
+			.insertOne(
+					new Document()
+					.append("id", id)
+					.append("name", name));
 	}
 }
