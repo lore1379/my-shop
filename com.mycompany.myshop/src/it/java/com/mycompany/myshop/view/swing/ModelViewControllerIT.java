ee051df808770c3014fc595e56b3d705da2149ba
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-10-19 11:40:10 +0200

    IT for Add to Cart now leads to a change in database
    Closes #4

--- com.mycompany.myshop/src/it/java/com/mycompany/myshop/view/swing/ModelViewControllerIT.java
+++ com.mycompany.myshop/src/it/java/com/mycompany/myshop/view/swing/ModelViewControllerIT.java
@@ -1,16 +1,24 @@
 package com.mycompany.myshop.view.swing;
 
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.bson.Document;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mycompany.myshop.controller.ShopController;
+import com.mycompany.myshop.model.Cart;
+import com.mycompany.myshop.model.Product;
 import com.mycompany.myshop.repository.mongo.ShopMongoRepository;
 
 @RunWith(GUITestRunner.class)
@@ -25,6 +33,9 @@ public class ModelViewControllerIT extends AssertJSwingJUnitTestCase {
 	private ShopSwingView shopSwingView;
 	private ShopController shopController;
 	private FrameFixture window;
+	
+	private MongoCollection<Document> productCollection;
+	private MongoCollection<Document> cartCollection;
 
 	@Override
 	protected void onSetUp() throws Exception {
@@ -34,6 +45,8 @@ public class ModelViewControllerIT extends AssertJSwingJUnitTestCase {
 				"shop", "product", "cart");
 		MongoDatabase database = mongoClient.getDatabase("shop");
 		database.drop();
+		productCollection = database.getCollection("product");
+		cartCollection = database.getCollection("cart");
 		GuiActionRunner.execute(() -> {
 			shopSwingView = new ShopSwingView();
 			shopController = new ShopController(shopSwingView, shopRepository);
@@ -50,6 +63,24 @@ public class ModelViewControllerIT extends AssertJSwingJUnitTestCase {
 	}
 
 	@Test
-	public void test() {
+	public void testAddToCart() {
+		Cart cart = new Cart("10");
+		cart.addToCart(new Product("1", "test1"));
+		productCollection.insertMany(asList(
+				new Document()
+				.append("id", "1")
+				.append("name", "test1"),
+				new Document()
+				.append("id", "2")
+				.append("name", "test2")));
+		cartCollection.insertOne(
+				new Document()
+				.append("id", "10"));
+		GuiActionRunner.execute( () ->
+			shopController.allProducts());
+		window.list("productList").selectItem(0);
+		window.button(JButtonMatcher.withText("Add to Cart")).click();
+		assertThat(shopRepository.findCart("10"))
+			.isEqualTo(cart);
 	}
 }
