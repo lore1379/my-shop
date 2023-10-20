e064baa0f146695845f01b701caac57c905d15d8
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-10-20 12:06:01 +0200

    Refactor - usage of Filters and Updates
    Define a constant instead of duplicating this literal "productList"

--- com.mycompany.myshop/src/main/java/com/mycompany/myshop/repository/mongo/ShopMongoRepository.java
+++ com.mycompany.myshop/src/main/java/com/mycompany/myshop/repository/mongo/ShopMongoRepository.java
@@ -19,6 +19,8 @@ import com.mycompany.myshop.repository.ShopRepository;
 public class ShopMongoRepository implements ShopRepository {
 
 
+	private static final String PRODUCT_LIST_IN_CART = "productList";
+	
 	private MongoCollection<Document> productCollection;
 	private MongoCollection<Document> cartCollection;
 
@@ -52,7 +54,7 @@ public class ShopMongoRepository implements ShopRepository {
 		Document d = cartCollection.find(Filters.eq("id", id)).first();
 		if (d != null) {
 			Cart cart = new Cart("" + d.get("id"));
-			List<Document> productList = d.getList("productList", Document.class);
+			List<Document> productList = d.getList(PRODUCT_LIST_IN_CART, Document.class);
 			if (productList != null) {
 				for (Document productDocument : productList) {
 		            cart.addToCart(fromDocumentToProduct(productDocument));
@@ -68,22 +70,21 @@ public class ShopMongoRepository implements ShopRepository {
 		Product productToAdd = fromDocumentToProduct(
 				productCollection.find(Filters.eq("id", productId)).first());
 		productCollection.deleteOne(Filters.eq("id", productId));
-		Document filter = new Document("id", cartId);
-		Document update = new Document("$push", 
-        		new Document("productList", 
-        				new Document()
-        				.append("id", productToAdd.getId())
-        				.append("name", productToAdd.getName())));
-        cartCollection.updateOne(filter, update);
+		Bson cartFilter = Filters.eq("id", cartId);
+		Bson updateFilter = Updates.push(PRODUCT_LIST_IN_CART,
+				new Document()
+				.append("id", productToAdd.getId())
+				.append("name", productToAdd.getName()));
+        cartCollection.updateOne(cartFilter, updateFilter);
 	}
 
 	@Override
 	public void moveProductToShop(String cartId, String productId) {
 		Bson cartFilter = Filters.eq("id", cartId);
-		Bson updateFilter = Updates.pull("productList", Filters.eq("id", productId));
+		Bson updateFilter = Updates.pull(PRODUCT_LIST_IN_CART, Filters.eq("id", productId));
 		List<Document> productList = cartCollection.find(cartFilter)
 				.first()
-				.getList("productList", Document.class);
+				.getList(PRODUCT_LIST_IN_CART, Document.class);
 		// search the matching document and convert to product
 		Optional<Product> productOptional = productList.stream()
                 .filter(d -> d.get("id").equals(productId))
@@ -101,18 +102,17 @@ public class ShopMongoRepository implements ShopRepository {
 	
 	@Override
 	public Boolean productFoundInCart(String cartId, String productId) {
-		Document filter = new Document("id", cartId)
-                .append("productList", 
-                		new Document("$elemMatch", new Document("id", productId)));
-		return cartCollection.find(filter).first() != null;
+		Bson cartFilter = Filters.and(
+				Filters.eq("id", cartId),
+                Filters.elemMatch(PRODUCT_LIST_IN_CART, Filters.eq("id", productId)));
+		return cartCollection.find(cartFilter).first() != null;
 	}
 
 	@Override
 	public void delete(String cartId, String productId) {
-		Document filter = new Document("id", cartId);
-        Document update = new Document("$pull", 
-        		new Document("productList", new Document("id", productId)));
-        cartCollection.updateOne(filter, update);
+		Bson cartFilter = Filters.eq("id", cartId);
+		Bson updateFilter = Updates.pull(PRODUCT_LIST_IN_CART, Filters.eq("id", productId));
+        cartCollection.updateOne(cartFilter, updateFilter);
 	}
 	
 	private Product fromDocumentToProduct(Document d) {
